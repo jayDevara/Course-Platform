163c3fd733f012d62dcb91ae79afd5f70e056a75
jay Dev <164337073+jayDevara@users.noreply.github.com>
2025-07-24 17:44:16 +0530

    Delete src directory

--- src/emails/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Email, EmailVerificationEvent
-
-admin.site.register(Email)
-admin.site.register(EmailVerificationEvent)
