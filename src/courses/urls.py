01f97c94398eb8b844cb92fc108257074f86fbc4
jay Dev <164337073+jayDevara@users.noreply.github.com>
2025-07-24 17:21:11 +0530

    Delete src directory

--- src/courses/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("<slug:course_id>/lessons/<slug:lesson_id>/", views.lesson_detail_view),
-    path("<slug:course_id>/", views.course_detail_view),
-    path("", views.course_list_view),
-]
